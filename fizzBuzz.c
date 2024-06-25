3076f61211f8512c810c8910407652fed39fd82c
Wiktorro <48845694+Wiktorro@users.noreply.github.com>
2024-06-25 16:52:03 +0200

    Fix fpermissive C errors

--- fizzBuzz.c
+++ fizzBuzz.c
@@ -17,8 +17,7 @@ int parse_int(char*);
 
 /*
  * Complete the 'fizzBuzz' function below.
- *
- * The function accepts INTEGER n as parameter.
+ * The function accepts INTEGER n as parameter, iterates [1,N]
  */
 
 void fizzBuzz(unsigned int n) {
@@ -35,8 +34,10 @@ void fizzBuzz(unsigned int n) {
 
 int main()
 {
-    int n = parse_int(ltrim(rtrim(readline())));
+    const int n = parse_int(ltrim(rtrim(readline())));
+
     fizzBuzz(n);
+
     return 0;
 }
 
@@ -44,7 +45,7 @@ char* readline() {
     size_t alloc_length = 1024;
     size_t data_length = 0;
 
-    char* data = malloc(alloc_length);
+    char* data = (char*) malloc(alloc_length);
 
     while (true) {
         char* cursor = data + data_length;
@@ -62,10 +63,10 @@ char* readline() {
 
         alloc_length <<= 1;
 
-        data = realloc(data, alloc_length);
+        data = (char*) realloc(data, alloc_length);
 
         if (!data) {
-            data = '\0';
+            data = NULL;
             break;
         }
     }
@@ -73,26 +74,27 @@ char* readline() {
     if (data[data_length - 1] == '\n') {
         data[data_length - 1] = '\0';
 
-        data = realloc(data, data_length);
+        data = (char*) realloc(data, data_length);
 
         if (!data) {
-            data = '\0';
+            data = NULL;
         }
     } else {
-        data = realloc(data, data_length + 1);
+        data = (char*) realloc(data, data_length + 1);
 
         if (!data) {
-            data = '\0';
+            data = NULL;
         } else {
             data[data_length] = '\0';
         }
     }
+
     return data;
 }
 
 char* ltrim(char* str) {
     if (!str) {
-        return '\0';
+        return NULL;
     }
 
     if (!*str) {
@@ -102,13 +104,12 @@ char* ltrim(char* str) {
     while (*str != '\0' && isspace(*str)) {
         str++;
     }
-
     return str;
 }
 
 char* rtrim(char* str) {
     if (!str) {
-        return '\0';
+        return NULL;
     }
 
     if (!*str) {
