6f322e4dcd8663bd693dd59efacd3151863b6e02
Wiktorro <48845694+Wiktorro@users.noreply.github.com>
2024-07-10 21:47:33 +0200

    Add solution in C# (#3)
    Create fizzBuzz.cs

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -1,4 +1,5 @@
 using System;
+
 using System.Linq; // for Enumerable itaration
 
 class HelloWorld {
@@ -6,6 +7,7 @@ class HelloWorld {
     //Console.WriteLine("Hello World");
     Enumerable.Range(1, 16).ToList().ForEach(x =>
        {
+         const int rem3 = x % 3;
     Console.WriteLine($"{(x % 3 == 0 ? "Fizz" : "")}{(x % 5 == 0 ? "Buzz" : "")}{(x % 3 != 0 && x % 5 != 0 ? x.ToString() : "")}");
     } );
   }
