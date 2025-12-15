4432da7085280290de5cf88c5256ba0c52fcce68
Wiktor Mis <mwiktor023@gmail.com>
2025-12-15 07:12:43 +0100

    Implement optimal and cleaner FizzBuzz method (#26)

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -3,6 +3,22 @@ using System;
 using System.Linq; // for Enumerable itaration
 
 class HelloWorld {
+  static void FizzBuzzWide(int x) {
+        //  const 
+        bool rem3 = (0 == (x % 3));
+        bool rem5 = (0 == (x % 5));
+        if (rem3)
+            {           
+            if (rem5)
+                Console.WriteLine("FizzBuzz");
+            else
+                Console.WriteLine("Fizz");
+            }
+        else if (rem5)
+            Console.WriteLine("Buzz");
+        else
+            Console.WriteLine(x);
+  }
   static void Main() {
     //Console.WriteLine("Hello World");
     Enumerable.Range(1, 16).ToList().ForEach(x =>
