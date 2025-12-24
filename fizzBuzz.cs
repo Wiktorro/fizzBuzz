2ca7e81ab4dcbaf0d87b5fffa347d8255aee76e7
Wiktor Mis <mwiktor023@gmail.com>
2025-12-24 07:33:52 +0100

    Implement FizzBuzz in oneline manner (#25)

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -3,6 +3,10 @@ using System;
 using System.Linq; // for Enumerable itaration
 
 class HelloWorld {
+  static void FizzBuzzOneline(int x) {
+    int rem3 = x % 3;
+    int rem5 = x % 5;
+    Console.WriteLine($"{(rem3 == 0 ? "Fizz" : "")}{(rem5 == 0 ? "Buzz" : "")}{(rem3 != 0 && rem5 != 0 ? x.ToString() : "")}");
   static void FizzBuzzWide(int x) {
         //  const 
         bool rem3 = (0 == (x % 3));
