bbe6ea46802c73ca5c7401a5a9b5bd86ae0533e5
Wiktor Mis <mwiktor023@gmail.com>
2025-12-26 07:19:15 +0100

    Integrate Stopwatch for execution time tracking (#22)

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -1,6 +1,7 @@
 using System;
 
 using System.Linq; // for Enumerable itaration
+using System.Diagnostics; // time measurement
 
 class HelloWorld {
   static void FizzBuzzOneline(int x) {
@@ -25,11 +26,19 @@ class HelloWorld {
   }
   static void Main() {
     //Console.WriteLine("Hello World");
+        // var  // works as auto type
+        Stopwatch 
+        stopwatch = Stopwatch.StartNew();
     Enumerable.Range(1, 16).ToList().ForEach(x =>
        {
          const int rem3 = x % 3;
          const int rem5 = x % 5;
     Console.WriteLine($"{(rem3 == 0 ? "Fizz" : "")}{(rem5 == 0 ? "Buzz" : "")}{(rem3 != 0 && rem5 != 0 ? x.ToString() : "")}");
     } );
+        stopwatch.Stop();
+        // Display elapsed time in different formats
+        Console.WriteLine($"Elapsed Time: {stopwatch.Elapsed}"); // Full TimeSpan
+        Console.WriteLine($"Elapsed Milliseconds: {stopwatch.ElapsedMilliseconds} ms");
+        Console.WriteLine($"Elapsed Ticks: {stopwatch.ElapsedTicks} ticks");
   }
 }
